89207006cd73b6f6c27172415d0aa2aeeadc8087
Alex Ri <cabeacdc@gmail.com>
2023-09-14 13:10:24 -0600

    Extraer de base de datos y tabla completa

--- SinapsisSoft/index.py
+++ SinapsisSoft/index.py
@@ -1,7 +1,14 @@
 from tkinter import ttk
 from tkinter import *
+import pymysql.cursors
 
 class Product:
+    dbHost = 'localhost'
+    dbPort = 3306
+    dbUser = 'root'
+    dbPassword = 'Alexelpro27'
+    dbName = 'sinapsis_clients'
+
     def __init__(self, window):
         self.wind = window
         self.wind.title('Sinapsis Soft')
@@ -19,10 +26,38 @@ class Product:
 
         ttk.Button(frame, text = 'Guardar producto').grid(row = 3, columnspan = 2, sticky = W + E)
 
-        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree = ttk.Treeview(height = 10, columns = ("","","",""))
         self.tree.grid(row = 4, column = 0, columnspan = 2)
-        self.tree.heading('#0', text = 'Nombre', anchor = CENTER)
-        self.tree.heading('#1', text = 'Precio', anchor = CENTER)
+        self.tree.heading('#0', text = 'Numero de Contrato', anchor = CENTER)
+        self.tree.heading('#1', text = 'Nombre', anchor = CENTER)
+        self.tree.heading('#2', text = 'Saldo Anterior', anchor = CENTER)
+        self.tree.heading('#3', text = 'Saldo Actual', anchor = CENTER)
+        self.tree.heading('#4', text = 'Mensualidades', anchor = CENTER)
+
+        self.get_clients()
+
+    def run_procedures(self,query,parameters = ()):
+        MysqlCnx = pymysql.connect(host=self.dbHost,port=self.dbPort,
+                                user=self.dbUser,
+                                password=self.dbPassword,
+                                db=self.dbName,
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+        cursor = MysqlCnx.cursor()
+        cursor.execute(query,parameters)
+        response=cursor.fetchall()
+        return response
+    
+    def get_clients(self):
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+
+        #Obetniendo datos y pintandolos
+        query = 'SELECT * FROM cliente'
+        response = self.run_procedures(query)
+            
+        
 if __name__ == '__main__':
     window = Tk()
     application = Product(window)
