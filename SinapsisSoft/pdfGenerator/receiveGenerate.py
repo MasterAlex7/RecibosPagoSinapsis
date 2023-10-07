9eb9bd74387559c64da2fee0771491bbcabae49a
Alex Ri <cabeacdc@gmail.com>
2023-10-07 15:08:31 -0600

    Recibo de Pago terminado
    Solo se espera autorizacion para seguir

--- SinapsisSoft/pdfGenerator/receiveGenerate.py
+++ SinapsisSoft/pdfGenerator/receiveGenerate.py
@@ -2,14 +2,20 @@ import pdfkit
 import jinja2
 
 def crearPDF():
-    my_name = "SinapsisSoft"
-    my_address = "Calle 123"
-    my_city = "Bogota"
-    my_state = "Colombia"
-    my_zip = "12345"
-    my_phone = "123-456-7890"
+    client_name = "Sinapsis Software"
+    num_contrato = "365098"
+    today_date = "12/12/2020"
+    id_recibo = "12"
+    mensualidad_num = "9"
+    abono = "2000"
+    saldo_ant = "3000"
+    descuento = "0"
+    saldo_actual = "1000"
+    nom_acreedor = "Sinapsis Software"
 
-    context = {'my_name': my_name,'my_address': my_address}
+    context = {'client_name': client_name, 'num_contrato': num_contrato, 'today_date': today_date,
+            'id_recibo': id_recibo, 'mensualidad_num': mensualidad_num, 'abono': abono, 
+            'saldo_ant': saldo_ant, 'descuento': descuento, 'saldo_actual': saldo_actual,'nom_acreedor':nom_acreedor}
 
     templateLoader = jinja2.FileSystemLoader('./')
     templateEnv=jinja2.Environment(loader=templateLoader)
@@ -19,7 +25,7 @@ def crearPDF():
     outputText=template.render(context)
 
     config = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
-    output_pdf = 'RecibodePagoSInapsis'+my_zip+'.pdf'
+    output_pdf = 'RecibodePagoSInapsis'+id_recibo+'.pdf'
     pdfkit.from_string(outputText, output_pdf, configuration=config, css='SinapsisSoft/pdfGenerator/styles.css')
 
 if __name__ == '__main__':
